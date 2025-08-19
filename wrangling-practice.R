f6002a4988491fbe2cf0250578d51072714c0cbd
avarobillard <avamorganr@gmail.com>
2025-08-19 11:46:55 -0700

    joins

--- wrangling-practice.R
+++ wrangling-practice.R
@@ -25,5 +25,55 @@ penguins %>%
   summarize(mean_flipper = mean(flipper_length_mm),
             sd_flipper = sd(flipper_length_mm),
             n_flipper = n())
-  
-  
+
+# Mutating joins
+
+animals <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("lagoon", "bluff", "creek", "oaks", "bluff"),
+                      species = c("bobcat",
+                                  "coyote","fox","squirrel","bobcat"),
+                     maturity = c("adult",
+                                  "juvenile","adult","juvenile","adult")
+           )
+
+sites <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("beach", "lagoon", "bluff", "oaks"),
+    full_site_name = c("Goleta Beach","UCSB Lagoon",
+                       "Ellwood Mesa","Fremont Campground"),
+      jurisdiction = c("SB City", "UCSB", "SB City", "USFS")
+)
+
+# practice with full_join
+# keeps all rows and adds all columns
+full_join(animals, sites)
+
+# left join
+# adds all columns and rows for matching locations in sites
+left_join(animals, sites)
+
+# right join
+# adds all columns and rows for matching locations in animals
+right_join(animals, sites)
+
+# inner join
+# no NAs, full matches 
+inner_join(animals, sites)
+
+# Filtering joins
+
+# semi join
+# no new variables- just filtering rows
+semi_join(animals, sites)
+
+# same as this code:
+animals %>% 
+  filter(location %in% sites$location)
+
+# anti join
+anti_join(animals, sites)
+
+# same as this code:
+animals %>% 
+  filter(!location %in% sites$location)
